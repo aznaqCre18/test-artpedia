10e0fd710e76e668d5281488d430b68465cc6a61
aznaqCre18 <azizaqibs@gmail.com>
2022-02-11 22:42:33 +0700

    add my own structure folder

--- rating-use-react/src/App.js
+++ rating-use-react/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-const App = () => {
-  return (
-    <div>Boiler plate</div>
-  )
+export default class App extends Component {
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
 }
-
-export default App;
